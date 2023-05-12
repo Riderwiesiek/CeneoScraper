3104d2672740e0a472d67c3b9de6d118d91d2832
Riderwiesiek <wiesiuhor@gmail.com>
2023-05-12 12:55:33 +0200

    analyzer

--- README.md
+++ README.md
@@ -17,4 +17,4 @@
 
 ## Wykorzystane biblioteki python'a
 - Request
-- Beautifulsoup
+- BeautifulSoup
--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import os
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,7 +12,7 @@ def get_cos(ancestor, selector=None, attribute=None, return_list=False):
         if attribute:
             return ancestor.select_one(selector)[attribute].strip()
         return ancestor.select_one(selector).get_text().strip()
-    except (ArithmeticError, TypeError):
+    except (AttributeError, TypeError):
         return None    
 
 selector = {
@@ -28,18 +30,34 @@ selector = {
             "cons": ("div.review-feature__title--negatives ~ div.review-feature__item", None, True)
 }
 
+
 #product_code = input("Podaj kod produktu: ")
-product_code = "58835954"
+#product_code = "58835954"
+product_code = "39562616"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
-respons = requests.get(url)
-if  respons.status_code == requests.codes.ok:
-    page_dom = BeautifulSoup(respons.text, 'html.parser')
-    opinions = page_dom.select("div.js_product-review")
-    opinions_all = []
-    for opinion in opinions:
-        single_opinion = {}
-        for key, value in selector.items():
-            single_opinion[key] = get_cos(opinion, *value)
-        
-        opinions_all.append(single_opinion)
-    print(opinions_all)
+opinions_all = []
+while(url):
+    print(url)
+    respons = requests.get(url)
+    if  respons.status_code == requests.codes.ok:
+        page_dom = BeautifulSoup(respons.text, 'html.parser')
+        opinions = page_dom.select("div.js_product-review")
+        for opinion in opinions:
+            single_opinion = {}
+            for key, value in selector.items():
+                single_opinion[key] = get_cos(opinion, *value)
+            opinions_all.append(single_opinion)
+    try:        
+        url ="https://www.ceneo.pl"+get_cos(page_dom,"a.pagination__next","href")
+    except TypeError:
+        url = None
+try:
+    os.mkdir("opinions")
+except FileExistsError:
+    pass
+#jf = open("opinions/{product_code}.json", "w", encoding="UTF-8")
+#json.dump(opinions_all, jf, indent=4, ensure_ascii=False)
+#jf.close()
+
+with open(f"opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
+    json.dump(opinions_all, jf, indent=4, ensure_ascii=False)
